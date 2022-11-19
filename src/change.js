fd7a1136fa3553c9ce6c10903e61aeb64db37925
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-11-19 18:54:58 +0530

    feat : Change

---
+++ src/change.js
@@ -0,0 +1,36 @@
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
+
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const iconToggle = () => {
+  moonIcon?.classList.toggle("display-none");
+  sunIcon?.classList.toggle("display-none");
+};
+
+const themeCheck = () => {
+  if (userTheme === "dark" || (!userTheme && systemTheme)) {
+    document.documentElement.classList.add("dark");
+    moonIcon.classList.add("display-none");
+  }
+  sunIcon.classList.add("display-none");
+};
+
+const themeSwitch = () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "dark");
+    iconToggle();
+  }
+};
+
+sunIcon?.addEventListener("click", () => {
+  themeSwitch();
+});
+
+moonIcon?.addEventListener("click", () => {
+  themeSwitch();
+});
+
+themeCheck();
--- src/component/Head.tsx
+++ src/component/Head.tsx
@@ -2,12 +2,12 @@ import { HiMoon, HiOutlineSun } from "react-icons/hi";
 
 const Head = () => {
   return (
-    <div className="h-screen flex items-center justify-center bg-slate-100 px-8">
-      <div className="max-w-[600px] shadow-md p-6 rounded-md bg-white">
+    <div className="h-screen flex items-center justify-center bg-slate-100 px-8 dark:bg-slate-800">
+      <div className="max-w-[600px] shadow-md p-6 rounded-md bg-white dark:bg-slate-700 dark:text-white">
         <div className="flex items-start sm:items-center mb-4">
           <h1 className="text-4xl flex-1 font-semibold">Light / Dark Mode</h1>
-          <HiOutlineSun className="hidden" size={30} />
-          <HiMoon size={30} />
+          <HiOutlineSun className="sun" size={30} />
+          <HiMoon className="moon" size={30} />
         </div>
         <p className="text-lg font-medium mb-4">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis cum
@@ -15,10 +15,11 @@ const Head = () => {
           necessitatibus aliquid amet placeat fugiat quod ducimus, sed aliquam
           blanditiis. Pariatur, neque.
         </p>
-        <button className="py-2 px-4 bg-black text-white rounded-md">
+        <button className="py-2 px-4 bg-black text-white rounded-md dark:bg-white dark:text-slate-800">
           Learn More !
         </button>
       </div>
+      <script src="../change.js"></script>
     </div>
   );
 };
